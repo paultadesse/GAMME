ec041e22b6f25c4acf21b68dcf7b87cb779c9f52
Paulos Tadesse <paulosta952@gmail.com>
2022-08-21 13:40:33 +0300

    :rocket: add animate css

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,60 @@
 <template>
-    <div>
-        <p class="text-4xl font-bold text-green-500">Gamme Home page</p>
+<div class="relative h-screen w-full bg-cover bg-fixed bg-center bg-no-repeat" style="background-image: url('https://artlogic-res.cloudinary.com/w_2000,h_2000,c_limit,f_auto,fl_lossy,q_auto/ws-addisfineart/usr/images/pages/slideshow_data/1/addisgezehagn_floatingcity_installphotography_june2022_addisfineart_lucyemms-16-.jpg')">
+  <div class="shadw-2xl absolute top-0 h-16 w-full text-black">
+    <!-- <div class="absolute inset-0 -z-0 bg-white opacity-0"></div> -->
+    <div class="flex h-full w-full items-center justify-between px-12">
+      <p class="z-10 text-3xl font-bold uppercase tracking-widest text-black">Gamme</p>
+      <ul class="justify-en h-full items-center text-sm font-light uppercase tracking-widest xl:flex xl:space-x-32">
+        <li class="z-10">Artists</li>
+        <li class="z-10">Exibition</li>
+        <li class="z-10">360° view</li>
+      </ul>
     </div>
+  </div>
+  <div class="absolute bottom-0 h-3/4">
+    <!-- <div class="absolute inset-0 -z-0 bg-white opacity-30"></div> -->
+    <div class="animate__animated animate__fadeInLeft flex h-full w-full flex-col justify-center px-12">
+      <div class="space-y-2 bg-gray-50 py-2 px-4 opacity-75">
+        <p class="z-10 text-xl font-light uppercase tracking-widest text-black">Artist</p>
+        <p class="z-10 text-3xl font-light uppercase tracking-widest text-black">paul tad | modern arts</p>
+        <p class="z-10 text-sm font-light uppercase tracking-widest text-black">Ethiopia is the origin of everything</p>
+      </div>
+    </div>
+  </div>
+  <div class="absolute bottom-0 z-10 w-full px-12">
+    <div class="flex items-end justify-end">
+      <div class="flex items-center space-x-7 bg-white px-4 py-2 text-xs uppercase tracking-widest text-black">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer transition delay-100 ease-in hover:scale-105" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+        </svg>
+
+        <p>|</p>
+
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer transition delay-100 ease-in hover:scale-105" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+        </svg>
+      </div>
+    </div>
+  </div>
+  <div class="absolute inset-0 -z-0 bg-black opacity-0"></div>
+</div>
+
+<div class="h-[700px] bg-blue-50">
+  <div class="h-1/2">first .....</div>
+  <div class="h-full w-full bg-cover bg-fixed" style="background-image: url('https://artlogic-res.cloudinary.com/w_2000,h_2000,c_limit,f_auto,fl_lossy,q_auto/ws-addisfineart/usr/images/pages/slideshow_data/1/merikokebberhanu_venicebienalle_installphotography_2022_highres-9-.jpg')"></div>
+  <!-- <div class=""><p>first</p></div> -->
+</div>
+
+<div class="h-[700px] bg-green-50">
+  <div class="h-1/2">middle .....</div>
+  <div class="h-full w-full bg-cover bg-fixed" style="background-image: url('https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1545&q=80')"></div>
+  <!-- <div class=""><p>first</p></div> -->
+</div>
+
+<div class="h-[700px] bg-red-50">
+  <div class="h-1/3">last .....</div>
+  <div class="h-full w-full bg-cover bg-fixed bg-center" style="background-image: url('https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"></div>
+  <div class="h-1/4"><p>last element</p></div>
+</div>
+
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
-
+import 'animate.css';
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
   setup({ el, App, props, plugin }) {
