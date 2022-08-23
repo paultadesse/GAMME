5fb816914de21003da336356208bcb52e726f5e6
Paulos Tadesse <paulosta952@gmail.com>
2022-08-23 18:35:47 +0300

    :zap: exhibition component for artist detail

--- public/js/resources_js_Components_Artist_Exhibition_vue.js
+++ public/js/resources_js_Components_Artist_Exhibition_vue.js
@@ -13,14 +13,45 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = {
+  "class": "grid grid-cols-4 xl:grid-cols-12 gap-4 bg-white"
+};
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", null, "Artist Exhibition ...", -1
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+  "class": "relative pb-full bg-gray-500 xs:h-auto xs:square animate__animated animate__fadeIn animate_delay-2s"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
+  "class": "absolute h-full w-full object-cover",
+  src: "https://uploads6.wikiart.org/images/afewerk-tekle/the-total-liberation-of-africa.jpg!Large.jpg",
+  alt: "",
+  srcset: ""
+})], -1
 /* HOISTED */
 );
 
-var _hoisted_2 = [_hoisted_1];
+var _hoisted_3 = {
+  "class": "p-12 py-1 text-sm tracking-widest font-light uppercase animate__animated animate__fadeInLeft"
+};
+var _hoisted_4 = {
+  "class": "text-center text-white"
+};
+
+var _hoisted_5 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h1", {
+  "class": ""
+}, "Afewerk Tekle", -1
+/* HOISTED */
+);
+
 function render(_ctx, _cache) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", null, _hoisted_2);
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)(8, function (exhibition) {
+    return (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+      "class": "col-span-2 w-96 xl:col-span-4 bg-black shadow-2xl hover:scale-105 hover:z-20 transition ease-in delay-100",
+      key: exhibition
+    }, [_hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [_hoisted_5, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", null, "exhibition " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(exhibition), 1
+    /* TEXT */
+    )])])]);
+  }), 64
+  /* STABLE_FRAGMENT */
+  ))]);
 }
 
 /***/ }),
--- resources/js/Components/Artist/Exhibition.vue
+++ resources/js/Components/Artist/Exhibition.vue
@@ -1,5 +1,37 @@
 <template>
-    <div>
-        <p>Artist Exhibition ...</p>
+  <div class="grid grid-cols-4  xl:grid-cols-12 gap-4 bg-white">
+    <div class="col-span-2 w-96 xl:col-span-4 bg-black shadow-2xl hover:scale-105 hover:z-20 transition ease-in delay-100" v-for="exhibition in 8" :key="exhibition">
+      <div
+        class="
+          relative
+          pb-full
+          bg-gray-500
+          xs:h-auto xs:square
+        animate__animated animate__fadeIn animate_delay-2s
+        "
+      >
+        <img
+          class="absolute h-full w-full object-cover"
+          src="https://uploads6.wikiart.org/images/afewerk-tekle/the-total-liberation-of-africa.jpg!Large.jpg"
+          alt=""
+          srcset=""
+        />
+      </div>
+      <div
+        class="p-12
+          py-1
+          text-sm
+          tracking-widest
+          font-light
+          uppercase
+          animate__animated animate__fadeInLeft
+        "
+      >
+        <div class="text-center text-white">
+          <h1 class="">Afewerk Tekle</h1>
+          <span>exhibition {{ exhibition }}</span>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
